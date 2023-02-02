913911224383e23f1355801693bbb1eb28cec5c5
Sachin <sachinbondre72@gmail.com>
2023-02-02 16:29:24 +0530

    Commit by Branch01

--- linkedlist.java
+++ linkedlist.java
@@ -1,39 +1,64 @@
 public class linkedlist{
 
     public Node head =null;
+    public Node tail = null;
     class Node{
         int data;
         Node next;
 
         public Node(){
-            data=0;
+            this.data=0;
             this.next=null;
 
         }
         public Node(int d){
-            data=d;
+            this.data=d;
             this.next=null;
-
         }
+    }
 
         public void addNode(int data){
             Node n = new Node(data);
-            Node temp = new Node();
-            temp=head;
+            
+            
             if(head==null)
             {
                 head=n;
-            }else{
-
+                tail=n;
+            }
+            else{
+                tail.next=n;
+                tail=n;
             }
         }
-    }
+
+        public void Display(){
+            Node temp = head;
+            if(head==null)
+            {
+                System.out.println("Linkedlist is Empty");
+            }else{
+                while(temp!=null)
+                {
+                    System.out.print(temp.data+"---->");
+                    temp=temp.next;
+                }
+                System.out.println();
+            }
+        }
+
 
 
 
     public static void main(String[] args){
         System.out.println("Hello");
+        linkedlist ssl = new linkedlist();
+        ssl.addNode(10);
+        ssl.addNode(20);
+        ssl.addNode(30);
+        ssl.addNode(40);
+        ssl.Display();
 
-        
+
     }
 }
